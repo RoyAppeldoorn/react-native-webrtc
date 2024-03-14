19f014bc32bf1a5fa1a1da97c9dfc71b74db594e
RoyAppeldoorn <roy.appeldoorn@gmail.com>
2024-03-14 13:57:04 +0100

    Adhere to webrtc spec

--- src/RTCIceCandidate.ts
+++ src/RTCIceCandidate.ts
@@ -6,14 +6,10 @@ interface RTCIceCandidateInfo {
 
 export default class RTCIceCandidate {
     candidate: string;
-    sdpMLineIndex: number;
-    sdpMid: string;
+    sdpMLineIndex: number | null;
+    sdpMid: string | null;
 
     constructor({ candidate = '', sdpMLineIndex = null, sdpMid = null }: RTCIceCandidateInfo) {
-        if (sdpMLineIndex === null || sdpMid === null) {
-            throw new TypeError('`sdpMLineIndex` and `sdpMid` must not null');
-        }
-
         this.candidate = candidate;
         this.sdpMLineIndex = sdpMLineIndex;
         this.sdpMid = sdpMid;
